c4ca19c7fd770664cb416bf3aa5f0706a1fd691e
kebaldwi <kebaldwi@cisco.com>
2023-10-09 10:05:39 -0400

    Refactoring Complete - MVP
    Refactoring complete MVP available.
    
    Outstanding Roadmap items
    1. Scheduler Service and Configuration
    2. Render SWAGGER API (bryn help)

--- app/configuration_template.py
+++ app/configuration_template.py
@@ -15,8 +15,8 @@ import socket
 # DNAC username and password, server info and file locations
 
 # Update this section with the DNA Center server info and user information
-DNAC_IP = '10.10.0.20'
-DNAC_USER = 'demouser'
+DNAC_IP = '10.10.10.10'
+DNAC_USER = 'user'
 DNAC_PASS = 'C1sco12345'
 DNAC_URL = 'https://' + DNAC_IP
 DNAC_FQDN = socket.getfqdn(DNAC_IP)
